4d9944233a4ebb948dc044d8c85f7e1bfcec105e
shining <shining2327@gamil.com>
2020-09-15 18:16:44 +0900

    type writer commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 
 
 
-
 document.addEventListener("DOMContentLoaded", function(){
     // Handler when the DOM is fully loaded
 
+    // change theme 
     document.getElementById('themeSwitch').addEventListener('change', function(event){
         (event.target.checked) ? document.body.setAttribute('secondary-theme', 'dark') :
         document.body.removeAttribute('secondary-theme');
     });
 
+
+    // nice scrolling
+    $("body").niceScroll({
+        cursorcolor:"#2ab9a5",
+        cursorwidth:"10px"
+    });
+
+    // $(".nicescroll-box").niceScroll(".wrap",{cursorcolor:"aquamarine"});
+
+
 });
     
-    
-   
